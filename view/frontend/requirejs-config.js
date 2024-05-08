5bf0abf23fadf53b61118645732edc959b5a4080
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2024-05-08 14:32:42 +0300

    Show ajax request error in modal popup. Closes #1

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,5 @@
+var config = {
+    deps: [
+        'Swissup_Ignition/js/ignition'
+    ]
+};
---
+++ view/frontend/web/js/ignition.js
@@ -0,0 +1,37 @@
+define([
+    'jquery',
+    'Magento_Ui/js/modal/modal'
+], function ($) {
+    'use strict';
+
+    $(document).on('ajaxError', async function (e, obj) {
+        var iframe;
+
+        if (!obj.responseText.includes('Ignition')) {
+            return;
+        }
+
+        iframe = $('<iframe style="border:0;width:100%;height:100%;"></iframe>');
+        iframe
+            .modal({
+                modalClass: 'modal-ignition',
+                autoOpen: true,
+                buttons: []
+            })
+            .closest('.modal-content')
+            .css({
+                flexGrow: 1
+            })
+            .closest('.modal-inner-wrap')
+            .css({
+                maxWidth: '1600px',
+                width: '100%',
+                height: '95%'
+            });
+
+        // do not use "srcdoc" or "src" for working anchor links
+        iframe[0].contentDocument.open();
+        iframe[0].contentDocument.write(obj.responseText);
+        iframe[0].contentDocument.close();
+    });
+});
