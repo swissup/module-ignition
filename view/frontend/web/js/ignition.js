7af5878a94ae10a8e033e39df13cf9d0392aeab5
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2024-05-24 12:22:20 +0300

    Do not require modal until its needed

--- view/frontend/web/js/ignition.js
+++ view/frontend/web/js/ignition.js
@@ -1,17 +1,11 @@
 define([
-    'jquery',
-    'Magento_Ui/js/modal/modal'
+    'jquery'
 ], function ($) {
     'use strict';
 
-    $(document).on('ajaxError', async function (e, obj) {
-        var iframe;
+    function openModal(content) {
+        var iframe = $('<iframe style="border:0;width:100%;height:100%;"></iframe>');
 
-        if (!obj.responseText?.includes('window.ignite(window.data)')) {
-            return;
-        }
-
-        iframe = $('<iframe style="border:0;width:100%;height:100%;"></iframe>');
         iframe
             .modal({
                 modalClass: 'modal-ignition',
@@ -31,7 +25,15 @@ define([
 
         // do not use "srcdoc" or "src" for working anchor links
         iframe[0].contentDocument.open();
-        iframe[0].contentDocument.write(obj.responseText);
+        iframe[0].contentDocument.write(content);
         iframe[0].contentDocument.close();
+    }
+
+    $(document).on('ajaxError', async function (e, obj) {
+        if (!obj.responseText?.includes('window.ignite(window.data)')) {
+            return;
+        }
+
+        require(['Magento_Ui/js/modal/modal'], () => openModal(obj.responseText));
     });
 });
