bdc28b08416786941503725497da3b0e8da40909
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2024-07-12 16:05:30 +0300

    Improve modal dimensions on Luma and Breeze themes.

--- view/base/web/js/ignition.js
+++ view/base/web/js/ignition.js
@@ -1,32 +1,47 @@
 define([
-    'jquery'
+    'jquery',
+    'domReady!'
 ], function ($) {
     'use strict';
 
-    function openModal(content) {
-        var iframe = $('<iframe style="border:0;width:100%;height:100%;"></iframe>');
+    document.head.appendChild(document.createElement('style')).innerHTML = `
+        .modal-ignition {
+            --ignition-modal-gap: 20px;
+            .breeze-theme & {
+                --ignition-modal-gap: 0px;
+            }
 
-        iframe
-            .modal({
-                modalClass: 'modal-ignition',
-                autoOpen: true,
-                buttons: []
-            })
-            .closest('.modal-content')
-            .css({
-                flexGrow: 1
-            })
-            .closest('.modal-inner-wrap')
-            .css({
-                maxWidth: '1600px',
-                maxHeight: 'none',
-                width: '100%',
-                height: '95%'
-            })
-            .closest('.modal-ignition')
-            .css({
-                zIndex: 999999
-            });
+            z-index: 999999;
+
+            .modal-inner-wrap {
+                left: 0 !important;
+                margin: var(--ignition-modal-gap) auto !important;
+                width: calc(100% - var(--ignition-modal-gap) * 2) !important;
+                height: calc(100% - var(--ignition-modal-gap) * 2) !important;
+                max-width: 1600px !important;
+                max-height: none !important;
+            }
+
+            .modal-content {
+                flex-grow: 1;
+            }
+
+            iframe {
+                border: 0;
+                width: 100%;
+                height: 100%;
+            }
+        }
+    `;
+
+    function openModal(content) {
+        var iframe = $('<iframe></iframe>');
+
+        iframe.modal({
+            modalClass: 'modal-ignition',
+            autoOpen: true,
+            buttons: []
+        });
 
         // do not use "srcdoc" or "src" for working anchor links
         iframe[0].contentDocument.open();
