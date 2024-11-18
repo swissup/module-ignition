5942090e2b151fada6a0be13a4bfd1997aa0d3c3
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2024-11-19 00:27:19 +0200

    Improve parsing of XMLValidationError

--- Solutions/SolutionProviders/XmlValidationErrorSolutionProvider.php
+++ Solutions/SolutionProviders/XmlValidationErrorSolutionProvider.php
@@ -16,14 +16,16 @@ class XmlValidationErrorSolutionProvider implements HasSolutionsForThrowable
 
     public function getSolutions(Throwable $throwable): array
     {
-        preg_match('/The XML in file "(?<filepath>.+?)" is invalid:\s+Element \'(?<element>.+?)\': (?<message>.+)\.\s+Line: (?<line>\d+)/', $throwable->getMessage(), $matches);
+        // The XML in file "/view/adminhtml/image.xml" is invalid: Element 'attribute': Duplicate key-sequence ['width'] in unique identity-constraint 'uniqueNames'. Line: 10 The xml was:
+        // The XML in file "/view/adminhtml/form.xml" is invalid: xmlParseEntityRef: no name Line: 120 xmlParseEntityRef: no name Line: 120 Verify the XML and try again.
+        preg_match('/The XML in file "(?<filepath>.+?)" is invalid:\s+(?<message>.+?)\s+Line: (?<line>\d+)/', $throwable->getMessage(), $matches);
 
         if (!$matches) {
             return [];
         }
 
         $message = strtr($matches['message'], [
-            "in unique identity-constraint 'uniqueNames'" => '',
+            "in unique identity-constraint 'uniqueNames'." => '',
         ]);
         $filepath = strtr($matches['filepath'], [
             BP => '',
