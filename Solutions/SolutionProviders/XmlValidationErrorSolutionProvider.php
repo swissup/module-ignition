fa12091e7af67981312493d27a5fd52881c6d034
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2024-11-26 09:48:18 +0200

    Improve output of XML validation errors

--- Solutions/SolutionProviders/XmlValidationErrorSolutionProvider.php
+++ Solutions/SolutionProviders/XmlValidationErrorSolutionProvider.php
@@ -11,19 +11,53 @@ class XmlValidationErrorSolutionProvider implements HasSolutionsForThrowable
 {
     public function canSolve(Throwable $throwable): bool
     {
-        return str_starts_with($throwable->getMessage(), 'The XML in file "');
+        return str_starts_with($throwable->getMessage(), 'The XML in file "')
+            || $throwable instanceof ValidationException;
     }
 
     public function getSolutions(Throwable $throwable): array
     {
+        $message = $throwable->getMessage();
+
         // The XML in file "/view/adminhtml/image.xml" is invalid: Element 'attribute': Duplicate key-sequence ['width'] in unique identity-constraint 'uniqueNames'. Line: 10 The xml was:
         // The XML in file "/view/adminhtml/form.xml" is invalid: xmlParseEntityRef: no name Line: 120 xmlParseEntityRef: no name Line: 120 Verify the XML and try again.
-        preg_match('/The XML in file "(?<filepath>.+?)" is invalid:\s+(?<message>.+?)\s+Line: (?<line>\d+)/', $throwable->getMessage(), $matches);
-
-        if (!$matches) {
-            return [];
+        preg_match('/The XML in file "(?<filepath>.+?)" is invalid:\s+(?<message>.+?)\s+Line: (?<line>\d+)/', $message, $matches);
+        if ($matches) {
+            return $this->createSolutionFromTheXmlInFileError($matches);
         }
 
+        preg_match('/(?<message>.+)\.\s+Line: (?<line>\d+)/', $message, $matches);
+        if ($matches) {
+            if (str_contains($message, 'The type definition is abstract')) {
+                $matches['message'] = 'Ensure that correct `xsi:type` is used';
+            } else if (str_contains($message, 'Expected is ( updater )')) {
+                // Element 'item': This element is not expected. Expected is ( updater ). Line: 1495 The xml was: 1490: <item ..
+                $matches['message'] = 'Check if proper `xsi:type` value is used for **parent element**';
+            }
+
+            $solution = sprintf('%s on line %s', $matches['message'], $matches['line']);
+
+            $startStr = $matches['line'] . ': ';
+            $endStr = $matches['line'] + 1 . ': ';
+            $startPos = strpos($message, $startStr);
+            $endPos = strpos($message, $endStr);
+            if ($startPos && $endPos) {
+                $line = trim(substr(
+                    $message,
+                    $startPos + strlen($startStr),
+                    $endPos - $startPos - strlen($startStr)
+                ));
+                $solution .= ': ' . $line;
+            }
+
+            return [
+                BaseSolution::create('XML validation error')->setSolutionDescription($solution),
+            ];
+        }
+    }
+
+    private function createSolutionFromTheXmlInFileError(array $matches)
+    {
         $message = strtr($matches['message'], [
             "in unique identity-constraint 'uniqueNames'." => '',
         ]);
